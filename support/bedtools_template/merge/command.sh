0f06c8b453a009ee3d886d770dcdc2c9e8ecbeac
Irffan Alahi <irffanalahi@Irffans-MacBook-Pro.local>
2020-09-29 09:48:14 -0500

    bedtools merge long distance

--- support/bedtools_template/merge/command.sh
+++ support/bedtools_template/merge/command.sh
@@ -1,3 +1,6 @@
 bedtools merge -d -1 -i a.bed > a_merged_onlyoverlapping.bed ### only overlapping, end same will be kept as it is
 
 bedtools merge  -i a.bed > a_merged_endsameoverlapping.bed ### also end same will be considered
+
+
+bedtools merge -d 600 -i b > b_600_merged.txt ##### merge with a long distance
