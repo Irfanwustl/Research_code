1faba65ee1bcb4cc311840bc7a25bab346932e63
Irfanwustl <irfan.alahi@wustl.edu>
2021-07-17 12:20:28 -0500

    exp

---
+++ in-silico-mix/tools/EXP_regionbasedbamfiles_perfect.sh
@@ -0,0 +1,59 @@
+region=$1
+
+
+
+./regionbasedbamfiles_perfect.sh ${region} iDC_sorted
+
+
+
+./regionbasedbamfiles_perfect.sh ${region} M2_sorted
+
+
+
+./regionbasedbamfiles_perfect.sh ${region} mNeu_sorted
+
+
+
+./regionbasedbamfiles_perfect.sh ${region}  mDC_sorted
+
+
+
+./regionbasedbamfiles_perfect.sh ${region}  M0_sorted
+
+
+
+./regionbasedbamfiles_perfect.sh ${region}  cB_sorted
+
+
+
+./regionbasedbamfiles_perfect.sh ${region}  cd4naive_sorted
+
+
+
+./regionbasedbamfiles_perfect.sh ${region}  cd8naive_sorted
+
+
+
+./regionbasedbamfiles_perfect.sh ${region}  nB_sorted
+
+
+./regionbasedbamfiles_perfect.sh ${region}  mono_sorted
+
+
+./regionbasedbamfiles_perfect.sh ${region}  M1_sorted
+
+
+./regionbasedbamfiles_perfect.sh ${region} PC_sorted
+
+
+./regionbasedbamfiles_perfect.sh ${region} NK_sorted
+
+
+
+
+
+
+
+
+
+
