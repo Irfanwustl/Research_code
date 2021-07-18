cf41bc3950ef34556bf525a98fa80ebd491f2af5
Irfanwustl <irfan.alahi@wustl.edu>
2021-07-18 15:27:54 -0500

    some

--- preproecess_for_sig_input/notmeteline/efficientnahandle/pipe/runmet/copy_files.py
+++ preproecess_for_sig_input/notmeteline/efficientnahandle/pipe/runmet/copy_files.py
@@ -12,6 +12,6 @@ if len(files) == 1:
 new_dir = old_dir[:-4]
 
 for fol in os.listdir(folder):
-    name = fol.split('\\')[1][1:]
+    name = fol.split('\\')[-1][1:]
     og = folder + '/' + fol + '/stdout'
     shutil.copyfile(og, new_dir + '/' + name + '.txt')
