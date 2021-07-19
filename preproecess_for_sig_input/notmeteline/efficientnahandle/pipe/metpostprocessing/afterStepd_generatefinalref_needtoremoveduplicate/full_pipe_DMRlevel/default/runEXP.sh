4f57d5fa81e43caa99dc6d3c1e674a8e519c7d04
Irfanwustl <irfan.alahi@wustl.edu>
2021-07-19 13:33:51 -0500

    some

---
+++ preproecess_for_sig_input/notmeteline/efficientnahandle/pipe/metpostprocessing/afterStepd_generatefinalref_needtoremoveduplicate/full_pipe_DMRlevel/default/runEXP.sh
@@ -0,0 +1,26 @@
+./pipegenerateFinalref_DMR.sh  BL19v2_all_matrixCin_nr0.4_imputed  BL19v2metin_out_mincpg2_q0.5_addedcol_q1e-05_maxCompartmentwisedelta-0.1 -1
+
+./pipegenerateFinalref_DMR.sh  BL19v2_all_matrixCin_nr0.4_imputed BL19v2metin_out_mincpg2_q0.5_addedcol_q1e-05_maxCompartmentwisedelta-0.2 -1
+
+
+./pipegenerateFinalref_DMR.sh  BL19v2_all_matrixCin_nr0.4_imputed  BL19v2metin_out_mincpg2_q0.5_addedcol_q1e-05_maxCompartmentwisedelta-0.3 -1
+
+
+./pipegenerateFinalref_DMR.sh  BL19v2_all_matrixCin_nr0.4_imputed  BL19v2metin_out_mincpg2_q0.5_addedcol_q1e-05_maxCompartmentwisedelta-0.4 -1
+
+
+./pipegenerateFinalref_DMR.sh  BL19v2_all_matrixCin_nr0.4_imputed  BL19v2metin_out_mincpg2_q0.5_addedcol_q1e-05_maxCompartmentwisedelta-0.5 -1
+
+
+./pipegenerateFinalref_DMR.sh  BL19v2_all_matrixCin_nr0.4_imputed  BL19v2metin_out_mincpg2_q0.5_addedcol_q1e-05_maxCompartmentwisedelta-0.6 -1
+
+
+./pipegenerateFinalref_DMR.sh  BL19v2_all_matrixCin_nr0.4_imputed  BL19v2metin_out_mincpg2_q0.5_addedcol_q1e-05_maxCompartmentwisedelta-0.7 -1
+
+./pipegenerateFinalref_DMR.sh  BL19v2_all_matrixCin_nr0.4_imputed  BL19v2metin_out_mincpg2_q0.5_addedcol_q1e-05_maxCompartmentwisedelta-0.8 -1
+
+./pipegenerateFinalref_DMR.sh  BL19v2_all_matrixCin_nr0.4_imputed  BL19v2metin_out_mincpg2_q0.5_addedcol_q1e-05_maxCompartmentwisedelta-0.9 -1
+
+
+./pipegenerateFinalref_DMR.sh  BL19v2_all_matrixCin_nr0.4_imputed   BL19v2metin_out_mincpg2_q0.5_addedcol_q1e-05_maxCompartmentwisedelta-1.0 -1
+
