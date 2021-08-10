74a56ee72bbc661c4c0d4fbd2b6e885909847c89
Irfanwustl <irfan.alahi@wustl.edu>
2021-08-10 07:57:23 -0500

    many

---
+++ NewDMRalgo/PELT_on_metdmer/run_PELT_onmetDMR3_python3.8.sh
@@ -0,0 +1,37 @@
+start=$SECONDS
+
+infolder=$1
+penaltyvalue=$2 #3.5 
+percoreMetDMR=$3 #1000
+
+minCpG=3
+
+
+outdir=${infolder}_PELT_minsegesize_${minCpG}_penaltyvalue_${penaltyvalue}_percoreMetDMR_${percoreMetDMR}
+
+seginfooutdir=${outdir}_segmentinfo
+
+dirList=($( ls ${infolder}  ))
+
+
+
+mkdir ${outdir}
+
+mkdir ${seginfooutdir}
+
+
+i=0
+while (( i < ${#dirList[@]} ))
+do
+
+	echo nowPELT=========${dirList[i]} 
+
+	python3.8 PELT_onmetDMR3.py ${infolder}/${dirList[i]} ${minCpG} ${penaltyvalue} ${percoreMetDMR}  ${outdir}/${dirList[i]}  ${seginfooutdir}/${dirList[i]} ${outdir}/${dirList[i]}_combined.txt ${seginfooutdir}/${dirList[i]}_combined.txt
+ 	(( i++ ))
+done
+
+
+end=$SECONDS
+duration=$(( end - start ))
+echo "stuff took $duration seconds to complete"
+
