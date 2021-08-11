7546a4d938d8d0cc39f3d66bcda104a0604908ac
Irfanwustl <irfan.alahi@wustl.edu>
2021-08-11 07:35:48 -0500

    run bam sort

---
+++ in-silico-mix/tools/runBAMsort.sh
@@ -0,0 +1,37 @@
+./bam_sort_flexi_job.sh Er
+
+
+./bam_sort_flexi_job.sh Mg
+
+
+./bam_sort_flexi_job.sh m4
+
+
+./bam_sort_flexi_job.sh Tr
+
+
+./bam_sort_flexi_job.sh Eo
+
+
+./bam_sort_flexi_job.sh DC
+
+
+./bam_sort_flexi_job.sh m8
+
+
+./bam_sort_flexi_job.sh mNeu
+
+
+./bam_sort_flexi_job.sh Mn
+
+
+./bam_sort_flexi_job.sh NK
+
+
+./bam_sort_flexi_job.sh nB
+
+
+./bam_sort_flexi_job.sh CD8
+
+
+./bam_sort_flexi_job.sh CD4
