65c7aec7dfd8f91b4a1f63137c546d43458ea982
Irfanwustl <irfan.alahi@wustl.edu>
2021-10-19 18:29:25 -0500

    many

--- likephased-seq/Background_related /background_pattern/PosFragment_hypoCpg_distribution_pipe/run_PosFragHYpocpg.sh
+++ likephased-seq/Background_related /background_pattern/PosFragment_hypoCpg_distribution_pipe/run_PosFragHYpocpg.sh
@@ -11,13 +11,13 @@ python3  PosFragment_hypoCpg_distribution.py mNeu1to.1_mincpg5
 
 
 
-python3  PosFragment_hypoCpg_distribution.py  BL14_atleast.2_top-1_singleCpG_final_assignedref_uniq.txt_result_final.txt_folder_mincpg1
+#python3  PosFragment_hypoCpg_distribution.py  BL14_atleast.2_top-1_singleCpG_final_assignedref_uniq.txt_result_final.txt_folder_mincpg1
 
 
-python3  PosFragment_hypoCpg_distribution.py  BL14_atleast.2_top-1_singleCpG_final_assignedref_uniq.txt_result_final.txt_folder_mincpg3
+#python3  PosFragment_hypoCpg_distribution.py  BL14_atleast.2_top-1_singleCpG_final_assignedref_uniq.txt_result_final.txt_folder_mincpg3
 
 
-python3  PosFragment_hypoCpg_distribution.py  BL14_atleast.2_top-1_singleCpG_final_assignedref_uniq.txt_result_final.txt_folder_mincpg5
+#python3  PosFragment_hypoCpg_distribution.py  BL14_atleast.2_top-1_singleCpG_final_assignedref_uniq.txt_result_final.txt_folder_mincpg5
 
 
 end=$SECONDS
